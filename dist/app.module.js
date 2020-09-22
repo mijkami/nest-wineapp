6a80ad5937e83971fbbc4440a8ed4131331cdc2d
mijkami <dimitri.gaillot@gmail.com>
2020-09-22 11:50:47 +0200

    implementing passport auth + jwt strategy

--- dist/app.module.js
+++ dist/app.module.js
@@ -10,11 +10,14 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const config_1 = require("@nestjs/config");
+const auth_module_1 = require("./auth/auth.module");
+const users_module_1 = require("./users/users.module");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     common_1.Module({
-        imports: [],
+        imports: [config_1.ConfigModule.forRoot(), auth_module_1.AuthModule, users_module_1.UsersModule],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
