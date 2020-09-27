4be35e6be48f7969cdd140dab0156b661aae2b23
WILMES <maxencewilmes@gmail.com>
2020-09-27 12:18:01 +0200

    first commit

--- src/interfaces/user.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface IUser extends Document {
-  readonly user: string;
-  readonly role: string;
-  readonly pass: string;
-  readonly firstName: string;
-  readonly lastName: string;
-}
--- src/schemas/user.schema.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from "mongoose";
-
-export const UserSchema = new mongoose.Schema({
-  name: String,
-  role: String,
-  pass: String,
-  firstName: String,
-  lastName: String
-})
