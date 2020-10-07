20b9fd975fe529b6440171aab7aff84b17c3cc7a
WILMES <maxencewilmes@gmail.com>
2020-10-07 15:05:25 +0200

    spaces imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {MongooseModule} from "@nestjs/mongoose";
+import { MongooseModule } from "@nestjs/mongoose";
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-import {ConfigModule} from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
 
 
 @Module({
