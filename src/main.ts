3937e052b69540e2b2f102e93e8a2c00dd5854d2
WILMES <maxencewilmes@gmail.com>
2020-10-12 15:40:37 +0200

    mongoose goes to WineApp instead of /test + helmet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.enableCors();
 
   const options = new DocumentBuilder()
     .setTitle('WineApp REST API')
