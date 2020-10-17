dd7480e2e29925bb820b7ff6b3b9cb0128a0859b
Mijka <dimitri.gaillot@gmail.com>
2020-10-17 13:49:17 +0200

    add new visible folder : api/docs

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
 
   app.enableCors();
   app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useStaticAssets(join(__dirname, '..', 'api/docs'));
 
   const options = new DocumentBuilder()
     .setTitle('WineApp REST API')
