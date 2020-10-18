1c06d47e947d47ee6825caf8ee9e2b2463ac4def
Mijka <dimitri.gaillot@gmail.com>
2020-10-18 14:09:59 +0200

    swagger change

--- src/main.ts
+++ src/main.ts
@@ -10,21 +10,24 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
+  app.setGlobalPrefix('api');
 
   app.use(helmet());
 
   app.enableCors();
   app.useStaticAssets(join(__dirname, '..', 'public'));
+  
 
   const options = new DocumentBuilder()
     .setTitle('WineApp REST API')
     .setDescription('API for our WineApp application')
     .setVersion('1.0')
+    // .setBasePath('v1')
     .addTag('WineApp')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(3000);
 }
